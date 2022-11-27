86ab47f554777d2f29a68b17826eec4a6659399e
raphade95 <72515398+raphade95@users.noreply.github.com>
2022-11-27 23:59:14 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
+
+
+//reveal and play animation on page load and hide it after//
+
+window.addEventListener('load', function() {
+    this.document.getElementById('main-page').classList.add('hidden');
+    this.document.getElementById('anim').classList.remove('hidden');
+    this.document.body.style.backgroundColor = '#000';
+
+  setTimeout(() => {
+    this.document.getElementById('anim').classList.add('hidden');
+    this.document.getElementById('main-page').classList.remove('hidden');
+    this.document.body.style.backgroundColor = '';
+  }, 19000)
+})
+
+
+
+// Init
+var $ = jQuery;
+var animationTime = 20,
+    days = 7;
+ 
+$(document).ready(function(){
+
+    // timer arguments: 
+    //   #1 - time of animation in mileseconds, 
+    //   #2 - days to deadline
+
+    $('#progress-time-fill, #death-group').css({'animation-duration': animationTime+'s'});
+
+    var deadlineAnimation = function () {
+        setTimeout(function(){
+            $('#designer-arm-grop').css({'animation-duration': '1.5s'});
+        },0);
+
+        setTimeout(function(){
+            $('#designer-arm-grop').css({'animation-duration': '1s'});
+        },4000);
+
+        setTimeout(function(){
+            $('#designer-arm-grop').css({'animation-duration': '0.7s'});
+        },8000);
+
+        setTimeout(function(){
+            $('#designer-arm-grop').css({'animation-duration': '0.3s'});
+        },12000);
+
+        setTimeout(function(){
+            $('#designer-arm-grop').css({'animation-duration': '0.2s'});
+        },15000);
+    };
+
+    function timer(totalTime, deadline) {
+        var time = totalTime * 1000;
+        var dayDuration = time / deadline;
+        var actualDay = deadline;
+
+        var timer = setInterval(countTime, dayDuration);
+
+        function countTime() {
+            --actualDay;
+            $('.deadline-days .day').text(actualDay);
+
+            if (actualDay == 0) {
+                clearInterval(timer);
+                $('.deadline-days .day').text(deadline);
+            }
+        }
+    }
+
+    var deadlineText = function () {
+        var $el = $('.deadline-days');
+        var html = '<div class="mask-red"><div class="inner">' + $el.html() + '</div></div><div class="mask-white"><div class="inner">' + $el.html() + '</div></div>';
+        $el.html(html);
+    };
+
+    deadlineText();
+
+    deadlineAnimation();
+    timer(animationTime, days);
+
+    setInterval(function(){
+        timer(animationTime, days);
+        deadlineAnimation();
+
+        console.log('begin interval', animationTime * 1000);
+
+    }, animationTime * 1000);
+
+});
+
